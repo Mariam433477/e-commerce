cd0d95bdfb113d2ff0d9241dc848e0801a6ad137
Mariam433477 <mariam.ehabsarhan@gmail.com>
2025-10-03 13:53:10 +0300

    build design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,27 +3,40 @@ import Carousel from "react-bootstrap/Carousel";
 import img1 from "../assets/photo1.jpg";
 import CarouselItemCom from "./CarouselItemCom";
 import img2 from "../assets/photo2.jpg";
-import img4 from "../assets/photo4.jpg";
+import img3 from "../assets/photo3.jpg";
 
 export default function Header() {
+  const info = [
+    {
+      img: img1,
+      title: "First slide label",
+      subTitle: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+    },
+    {
+      img: img2,
+      title: "Second slide label",
+      subTitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      img: img3,
+      title: "Third slide label",
+      subTitle:
+        "Praesent commodo cursus magna, vel scelerisque nisl consectetur.",
+    },
+  ];
   return (
     <>
       <Carousel data-bs-theme="dark">
-        <CarouselItemCom
-          img={img1}
-          title="First slide label"
-          subTitle="Nulla vitae elit libero, a pharetra augue mollis interdum."
-        />
-        <CarouselItemCom
-          img={img2}
-          title="Second slide label"
-          subTitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-        />
-        <CarouselItemCom
-          img={img4}
-          title="Third slide label"
-          subTitle="Praesent commodo cursus magna, vel scelerisque nisl consectetur."
-        />
+        {info.map((slide, index) => {
+          return (
+            <CarouselItemCom
+              img={slide.img}
+              title={slide.title}
+              subTitle={slide.subTitle}
+              key={index}
+            />
+          );
+        })}
       </Carousel>
     </>
   );
